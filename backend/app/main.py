442f6934ce36bcf00abfb75847f1e214d437b4cf
KANOMATA <yamadanatsumi@KANOMATAnoMacBook-Pro.local>
2025-05-28 18:25:18 +0900

    競合解消

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,7 +1,6 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from .routers import diaries
-from .routers import accounts
 
 app = FastAPI()
 
@@ -15,4 +14,3 @@ app.add_middleware(
 
 # /api 以下に diaries ルーターをマウント
 app.include_router(diaries.router, prefix="/api")
-app.include_router(accounts.router, prefix="/api")
